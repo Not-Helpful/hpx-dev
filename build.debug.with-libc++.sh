4481d38799f5df8ffaca8cb635b7e03646a869ef
Your Name <you@example.com>
2026-08-17 15:16:50 -0500

    sync

---
+++ build.debug.with-libc++.sh
@@ -0,0 +1,24 @@
+VERSION=debug-libc++
+rm -rf build/$VERSION
+mkdir -p build
+mkdir -p build/$VERSION
+
+CXX=clang++ cmake  -B build/$VERSION \
+            -DCMAKE_INSTALL_PREFIX=$INSTALLS/hpx/$VERSION \
+            -DHPX_WITH_FETCH_ASIO=ON \
+            -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+            -DCMAKE_EXE_LINKER_FLAGS="-stdlib=libc++" \
+            -DCMAKE_SHARED_LINKER_FLAGS="-stdlib=libc++" \
+            -DHPX_WITH_MALLOC="system" \
+            -DCMAKE_BUILD_TYPE=Debug \
+            -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+            -DHPX_WITH_CXX_STANDARD=23 .
+
+
+mkdir -p $INSTALLS/hpx
+rm -rf $INSTALLS/hpx/$VERSION
+mkdir -p $INSTALLS/hpx/$VERSION
+
+cd build/$VERSION
+cmake --build . --target install
+cd ..
